1c6043bb8a69ef02c3139971ecbb8162e3f7fee9
Kyle Mckay <kymckay.dev@gmail.com>
2021-11-04 17:52:57 +0000

    Calculate summary statistics
    - Data seems weakly dispersed in relation to the mean.
      A SD of ~7 is ~17% of the overall range (0.13 - 41.87).
    - The data is positively skewed as can be seen from the
      mean having higher value than the median as well as the
      quantile values (median is much closer to minimum than
      maximum).

--- investigation.r
+++ investigation.r
@@ -3,3 +3,10 @@ wind_data = read.csv("wind.csv")[["x"]]
 
 # Histogram to summarise data
 hist(wind_data)
+
+# Summary statistics
+cat("Wind Speed Summary Statistics\n")
+cat("Mean:", mean(wind_data), "mph\n")
+cat("Standard deviation:", sd(wind_data), "mph\n")
+cat("Median:", median(wind_data), "mph\n")
+cat("Quantiles:", quantile(wind_data), "(all mph)\n")
